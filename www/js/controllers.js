007d737dc5a869d0872abcf0c11cdc6a725b220f
contactjorge <contactjorge@s2a-MBAir-1701a.local>
2016-10-28 13:00:32 -0500

    Edit of Controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,7 +7,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('firstAidAppCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,7 +15,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('tasksCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,7 +23,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('timersCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,7 +31,7 @@ function ($scope, $stateParams) {
 
 
 }])
-      
+
 .controller('searchByEmergencyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,45 +39,42 @@ function ($scope, $stateParams) {
 
 
 }])
-   
-.controller('searchByVitalsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('searchByVitalsCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-    /*
-.controller('searchByVitals', ['$scope', '$http', '$state',
         function($scope, $http, $state) {
         $http.get('http://50.172.239.7:34344/vitals.json').success(function(data) {
           $scope.vitals = data.vitals;
           $scope.whichvital=$state.params.aId;
           $scope.data = { showDelete: false, showReorder: false };
-    
+
           $scope.onItemDelete = function(item) {
             $scope.vitals.splice($scope.vitals.indexOf(item), 1);
           }
-    
+
           $scope.doRefresh =function() {
           $http.get('http://50.172.239.7:34344/vitals.json').success(function(data) {
               $scope.vitals = data;
-              $scope.$broadcast('scroll.refreshComplete'); 
+              $scope.$broadcast('scroll.refreshComplete');
             });
           }
-    
+
           $scope.toggleStar = function(item) {
             item.star = !item.star;
           }
-    
+
           $scope.moveItem = function(item, fromIndex, toIndex) {
             $scope.vitals.splice(fromIndex, 1);
             $scope.vitals.splice(toIndex, 0, item);
           };
-          
-        }]);*/
+
+        });
 
 }
 
 ])
-   
+
 .controller('searchBySymptomsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -85,7 +82,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -93,7 +90,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('palliativeCareCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -101,7 +98,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('howToGuidesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -109,7 +106,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('survivalGuidesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -117,7 +114,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('urgentCareCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -125,4 +122,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
